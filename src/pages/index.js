05305796ff988efee4adc4ec73679155478a4585
samsriti <samsriti111@gmail.com>
2023-04-29 19:06:37 +0545

    nav changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-
+import Navibar from "@/global_components/Navibar";
 export default function Home() {
   return (
     <>
+      <Navibar> </Navibar>
       <div>Index page</div>
     </>
   );
