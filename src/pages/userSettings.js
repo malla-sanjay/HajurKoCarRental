949efd1161e00ec50f5c41ec96ed841e62c017ca
malla-sanjay <mallasanjay44@gmail.com>
2023-04-29 17:50:49 +0545

    folder strucutred

---
+++ src/pages/userSettings.js
@@ -0,0 +1,12 @@
+import Navibar from "@/global_components/Navibar";
+import React from "react";
+
+const UserSettings = () => {
+  return (
+    <>
+      <Navibar />
+    </>
+  );
+};
+
+export default UserSettings;
